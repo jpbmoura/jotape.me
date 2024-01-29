8e8a7a3efc3386f4e81ce040e2bc433af18a6919
João Pedro Batista de Moura <jpbmoura@MacBook-Pro-de-Joao.local>
2024-01-29 18:45:30 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { type AppType } from "next/app";
+
+import { Lato } from "next/font/google";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+});
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <main className={lato.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
+};
+
+export default api.withTRPC(MyApp);
